ebf8df01302a6fc0638660376bb3b3812a7dd09b
sawyes <7061384@126.com>
2017-05-15 22:04:50 +0800

    merge confict

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -24,11 +24,7 @@
 <div class="docs-wrapper container">
 
 	<section class="sidebar">
-<<<<<<< HEAD
-=======
-		{{-- ad div --}}
-		<!-- <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=laravelcom" id="_carbonads_js"></script> -->
->>>>>>> 2d90988c5eed85a2567429d937ba183e6062e28f
+
 		{!! $index !!}
 	</section>
 
